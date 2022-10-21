0f15c2b8c43447702e8e22821dd9b9ca3d6c0b3c
Brandon Phillips <bphillips.uwp@gmail.com>
2022-10-21 14:45:20 -0500

    Modified html page

--- src/trivial-web-server.go
+++ src/trivial-web-server.go
@@ -5,13 +5,20 @@ import (
 	"net/http"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "I am an application running in Kubernetes. Now at Version 1.0")
+// func indexHandler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "I am an application running in Kubernetes. Now at Version 1.0")
 
-}
+// }
 
 func main() {
 	fmt.Println("Basic web server is starting on port 8080...")
-	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":8080", nil)
+	fs := http.FileServer(http.Dir("./html"))
+	http.Handle("/", fs)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// http.HandleFunc("/", indexHandler)
+	// http.ListenAndServe(":8080", nil)
 }
