ac045df2d02995c60aa04efc9cc694828b49ea0d
Brandon Phillips <bphillips.uwp@gmail.com>
2022-10-21 23:15:53 -0500

    Add imagepullsecret

--- src/trivial-web-server.go
+++ src/trivial-web-server.go
@@ -5,20 +5,14 @@ import (
 	"net/http"
 )
 
-// func indexHandler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "I am an application running in Kubernetes. Now at Version 1.0")
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello GitOps Con!")
+	fmt.Fprintf(w, "Release: Staging")
 
-// }
+}
 
 func main() {
 	fmt.Println("Basic web server is starting on port 8080...")
-	fs := http.FileServer(http.Dir("html/"))
-	http.Handle("/", fs)
-
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-	// http.HandleFunc("/", indexHandler)
-	// http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8080", nil)
 }
